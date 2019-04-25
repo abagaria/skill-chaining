f92c22d348d35f027e142249a1add439e9eae1ca
Akhil Bagaria <abagaria@hmc.edu>
2019-04-24 21:17:47 -0400

    maybe uniform random exploration in the beginning is better for learning initiation sets

--- simple_rl/agents/func_approx/dsc/OptionClass.py
+++ simple_rl/agents/func_approx/dsc/OptionClass.py
@@ -330,7 +330,7 @@ class Option(object):
 					step_number < self.max_steps and num_steps < self.timeout:
 
 				if random_action_selection:
-					if np.random.random() < 0.75:
+					if np.random.random() < 0.95:
 						action = np.random.uniform(-self.overall_mdp.action_space_bound(), self.overall_mdp.action_space_bound(), self.overall_mdp.action_space_size())
 					else:
 						action = self.solver.act(state.features(), evaluation_mode=False)
