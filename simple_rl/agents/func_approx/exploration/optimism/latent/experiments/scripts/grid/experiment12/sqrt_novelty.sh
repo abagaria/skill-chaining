aa233879118e688b939dea840d8ba2b63c854799
Akhil Bagaria <abagaria@hmc.edu>
2020-02-16 17:04:57 -0500

    minor bug fix in grid scripts

--- simple_rl/agents/func_approx/exploration/optimism/latent/experiments/scripts/grid/experiment12/sqrt_novelty.sh
+++ simple_rl/agents/func_approx/exploration/optimism/latent/experiments/scripts/grid/experiment12/sqrt_novelty.sh
@@ -7,4 +7,4 @@
  #$-tc 10
  . /home/abagaria/.bashrc
  . /home/abagaria/miniconda3/bin/activate base
- simple_rl/agents/func_approx/exploration/optimism/latent/experiments/experiment12.py --experiment_name="exp12" --run_title DQN-Sqrt-Novelty --seed $SGE_TASK_ID --episodes=100 --steps=200 --exploration_method="count-phi" --use_bonus_during_action_selection=True --eval_eps=0. --bonus_scaling_term="sqrt"
+ python -u simple_rl/agents/func_approx/exploration/optimism/latent/experiments/experiment12.py --experiment_name="exp12" --run_title DQN-Sqrt-Novelty --seed $SGE_TASK_ID --episodes=100 --steps=200 --exploration_method="count-phi" --use_bonus_during_action_selection=True --eval_eps=0. --bonus_scaling_term="sqrt"
