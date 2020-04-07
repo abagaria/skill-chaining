313f4f7f456c3272a1ffefb73718186774077585
Kshitij Sachan <kshitijsachan39@gmail.com>
2020-04-07 15:11:18 -0500

    added breakpoint

--- simple_rl/agents/func_approx/dsc/OptionClass.py
+++ simple_rl/agents/func_approx/dsc/OptionClass.py
@@ -263,6 +263,7 @@ class Option(object):
 
 		# TODO: Kshitij deleted
 		# features = ground_state.features()[:2] if isinstance(ground_state, State) else ground_state[:2]
+		pdb.set_trace()
 		features = ground_state.features() if isinstance(ground_state, State) else ground_state[:5]
 		try:
 			return self.initiation_classifier.predict([features])[0] == 1
