b342d270bf78b2f48d8168dfc81a4ad01df53047
nktmerch <nicholas_merchant@brown.edu>
2020-05-19 16:07:30 -0600

    breakpoints

--- simple_rl/agents/func_approx/dsc/OptionClass.py
+++ simple_rl/agents/func_approx/dsc/OptionClass.py
@@ -365,9 +365,6 @@ class Option(object):
 		positive_labels = [1] * positive_feature_matrix.shape[0]
 		negative_labels = [0] * negative_feature_matrix.shape[0]
 
-		# TODO: The error is here, the dims of the entries in the positive and
-		# negative features matrices are different
-		pdb.set_trace()
 		X = np.concatenate((positive_feature_matrix, negative_feature_matrix))
 		Y = np.concatenate((positive_labels, negative_labels))
 
