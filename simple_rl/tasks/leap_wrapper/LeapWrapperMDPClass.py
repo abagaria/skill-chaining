95a697ddafdee656d376ac32c599810af9c02afb
kshitijsachan <kshitijsachan39@gmail.com>
2020-05-23 17:12:05 -0500

    breakpoints

--- simple_rl/tasks/leap_wrapper/LeapWrapperMDPClass.py
+++ simple_rl/tasks/leap_wrapper/LeapWrapperMDPClass.py
@@ -64,7 +64,6 @@ class LeapWrapperMDP(GoalDirectedMDP):
 
     def _reward_func(self, state, action):
         next_state, dense_reward, done, _ = self.env.step(action)
-        pdb.set_trace()
         if self.render:
             self.env.render()
         self.next_state = self._get_state(next_state, done)
