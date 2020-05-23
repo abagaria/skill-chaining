807e1fe1c1c791b4e351a9e446c0bec4d093931c
kshitijsachan <kshitijsachan39@gmail.com>
2020-05-23 14:35:25 -0500

    breakpoints

--- simple_rl/agents/func_approx/dsc/SalientEventClass.py
+++ simple_rl/agents/func_approx/dsc/SalientEventClass.py
@@ -57,6 +57,7 @@ class SalientEvent(object):
         return np.linalg.norm(position - target_position) <= self.tolerance
 
     def batched_is_init_true(self, position_matrix):
+        pdb.set_trace()
         assert isinstance(position_matrix, np.ndarray), type(position_matrix)
         goal_position = self.get_relevant_position(self.target_state)
         in_goal_position = distance.cdist(self.get_relevant_position(position_matrix), goal_position[None, :]) <= self.tolerance
