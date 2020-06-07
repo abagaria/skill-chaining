0036dfbf39623281e0020855429e18a9b1290152
kshitijsachan <kshitijsachan39@gmail.com>
2020-06-07 00:28:55 -0500

    changed epsilon start to 0.8

--- simple_rl/agents/func_approx/dsc/SkillChainingAgentClass.py
+++ simple_rl/agents/func_approx/dsc/SkillChainingAgentClass.py
@@ -144,7 +144,7 @@ class SkillChaining(object):
         # We start with this DQN Agent only predicting Q-values for taking the global_option, but as we learn new
         # options, this agent will predict Q-values for them as well
         self.agent_over_options = DQNAgent(self.mdp.state_space_size(), 1, trained_options=self.trained_options,
-                                           seed=seed, lr=1e-4, name="GlobalDQN", eps_start=0.8, tensor_log=tensor_log,
+                                           seed=seed, lr=1e-4, name="GlobalDQN", eps_start=0.5, tensor_log=tensor_log,
                                            use_double_dqn=True, writer=self.writer, device=self.device,
                                            exploration_strategy="shaping")
 
