5dccd943d005c918980e90a00b1dec4e458020c9
nktmerch <nicholas_merchant@brown.edu>
2020-06-10 12:47:04 -0600

    Debugging with ipdb.set_trace()

--- simple_rl/tasks/leap_wrapper/LeapWrapperMDPClass.py
+++ simple_rl/tasks/leap_wrapper/LeapWrapperMDPClass.py
@@ -33,8 +33,8 @@ class LeapWrapperMDP(GoalDirectedMDP):
             self.movie_framerate = 240.
             self.movie_timestep = 0
             self.movie_timestep_start = 0
-            self.movie_timestep_stop = 376
-            self.save_every = 376
+            self.movie_timestep_stop = 375
+            self.save_every = 375
 
             movie_duration = self.movie_timestep_stop - self.movie_timestep_start
             assert(self.save_every <= movie_duration)
