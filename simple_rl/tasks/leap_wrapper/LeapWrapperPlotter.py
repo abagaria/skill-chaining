6d6b50b2bb4203bae0d4679127ba7ff1506e752b
kshitijsachan <kshitijsachan39@gmail.com>
2020-06-15 18:39:37 -0500

    now plotting skill graphs

--- simple_rl/tasks/leap_wrapper/LeapWrapperPlotter.py
+++ simple_rl/tasks/leap_wrapper/LeapWrapperPlotter.py
@@ -104,8 +104,7 @@ class LeapWrapperPlotter(MDPPlotter):
 
             # plot quiver diagram
             ax.quiver(self.arrow_mesh[0], self.arrow_mesh[1], vectors[:, 0], vectors[:, 1], headlength=4, headwidth=2.6)
-            ipdb.set_trace()
-            self._plot_sawyer_features(ax, puck_pos=arrow_points[0][3:])
+            self._plot_sawyer_features(ax, puck_pos=tuple(arrow_points[0][3:]))
 
         # save plot
         file_name = f"{option.solver.name}_policy_seed_{seed}_episode_{episode}.png"
