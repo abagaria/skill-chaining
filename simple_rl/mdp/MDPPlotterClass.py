ba86a7047db25773b3078a3072d63861702bc2f5
kshitijsachan <kshitijsachan39@gmail.com>
2020-07-07 16:08:14 -0500

    set up learning curves fixing bugs

--- simple_rl/mdp/MDPPlotterClass.py
+++ simple_rl/mdp/MDPPlotterClass.py
@@ -55,6 +55,7 @@ class MDPPlotter(metaclass=abc.ABCMeta):
         print('*' * 80)
         # train learning curves and calculate average
         learning_curves = self.learning_curve(dsg_agent, episode, 1, True, 2)
+        ipdb.set_trace()
         mean = np.mean(learning_curves, axis=0)
         std_err = np.std(learning_curves, axis=0)
 
