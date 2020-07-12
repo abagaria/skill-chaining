9754178c679c12e5639ad578504bd179e2d0bb7d
Jason Senthil <jason@pop-os.localdomain>
2020-07-12 16:04:04 -0500

    dynamics improvements

--- simple_rl/agents/func_approx/dsc/dynamics/dynamics_model.py
+++ simple_rl/agents/func_approx/dsc/dynamics/dynamics_model.py
@@ -2,35 +2,68 @@ import torch
 import torch.nn as nn
 
 from torch.utils.data import DataLoader
-from simple_rl.agents.func_approx.dsc.dynamics.rollouts import collect_random_rollout
+from simple_rl.agents.func_approx.dsc.dynamics.rollouts import RandomRolloutCollector
 
 class DynamicsModel(nn.Module):
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size, action_size, mean_x, mean_y, mean_z, std_x, std_y, std_z):
         super(DynamicsModel, self).__init__()
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.set_standardization_vars(mean_x, mean_y, mean_z, std_x, std_y, std_z)
+
         self.model = nn.Sequential(
             nn.Linear(state_size + action_size, 500),
-            nn.ReLU(),
+            nn.LeakyReLU(),
+            nn.Linear(500, 500),
+            nn.LeakyReLU(),
             nn.Linear(500, state_size)
         )
     
+    def _numpy_to_torch(self, arr):
+        return torch.from_numpy(arr).to(self.device).float()
+    
     def forward(self, state, action):
+        state = (state - self.mean_x) / self.std_x
+        action = (action - self.mean_y) / self.std_y
         cat = torch.cat([state, action], dim=1)
         return self.model(cat)
+    
+    def set_standardization_vars(self, mean_x, mean_y, mean_z, std_x, std_y, std_z):
+        self.mean_x = self._numpy_to_torch(mean_x)
+        self.mean_y = self._numpy_to_torch(mean_y)
+        self.mean_z = self._numpy_to_torch(mean_z)
+        self.std_x = self._numpy_to_torch(std_x)
+        self.std_y = self._numpy_to_torch(std_y)
+        self.std_z = self._numpy_to_torch(std_z)
+    
+    def compare_state(self, state, action, state_p):
+        pred = self.forward(state, action)
+        return (pred * self.std_z) + self.mean_z + state, (state_p * self.std_z) + self.mean_z + state
 
 if __name__== "__main__":
     # use gpu if available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
-    model = DynamicsModel(6, 2)
+   
+    collector = RandomRolloutCollector(10)
+    train = collector.get_train_data()
+    test_sameloc = collector.construct_data(epochs=1, random_position=False)
+    test_diffloc = collector.construct_data(epochs=1, random_position=True)
+    training_gen = DataLoader(train, batch_size=32, shuffle=True, drop_last=True)
+    test_sameloc_gen = DataLoader(test_sameloc, batch_size=32, shuffle=False, drop_last=True)
+    test_diffloc_gen = DataLoader(test_diffloc, batch_size=32, shuffle=False, drop_last=True)
+
+    model = DynamicsModel(6, 2, *collector.get_standardization_vars())
     model.to(device)
     loss_function = nn.MSELoss().to(device)
-    rollout = collect_random_rollout(epochs=1)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    training_generator = DataLoader(rollout, batch_size=32, shuffle=True, drop_last=True)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    train_list = []
+    test_sameloc_list = []
+    test_diffloc_list = []
 
     for epoch in range(1, 1000):
         total_loss = 0
-        for states, actions, states_p in training_generator:
+        for states, actions, states_p in training_gen:
             states = states.to(device).float()
             actions = actions.to(device).float()
             states_p = states_p.to(device).float()
@@ -41,5 +74,54 @@ if __name__== "__main__":
             loss.backward()
             optimizer.step()
             
-            total_loss += loss
-        print("Epoch {} total loss {}".format(epoch + 1, total_loss))
+            total_loss += loss_function(*model.compare_state(states, actions, states_p))
+        
+        train_list.append(total_loss.cpu().detach().item())
+        
+        with torch.no_grad():
+            total_loss2 = 0
+            for states, actions, states_p in test_sameloc_gen:
+                states = states.to(device).float()
+                actions = actions.to(device).float()
+                states_p = states_p.to(device).float()
+                
+                # states_pred = model.forward(states, actions)
+                # loss = loss_function(states_p, states_pred) 
+                total_loss2 += loss_function(*model.compare_state(states, actions, states_p))
+            
+            test_sameloc_list.append(total_loss2.cpu().detach().item())
+            
+            test_loss = 0
+            for states, actions, states_p in test_diffloc_gen:
+                states = states.to(device).float()
+                actions = actions.to(device).float()
+                states_p = states_p.to(device).float()
+                
+                # states_pred = model.forward(states, actions)
+                # loss = loss_function(states_p, states_pred) 
+                test_loss += loss_function(*model.compare_state(states, actions, states_p))
+            
+            test_diffloc_list.append(test_loss.cpu().detach().item())
+        
+        print("Epoch {} total loss {}".format(epoch, total_loss / 10))
+        print("Epoch {} test loss {}".format(epoch, total_loss2))
+        print("Epoch {} test diff point loss {}".format(epoch, test_loss))
+        print("")
+    
+    import matplotlib.pyplot as plt
+    for i in range(len(train_list)):
+        train_list[i] = train_list[i] / 10
+    plt.figure()
+    plt.plot(train_list)
+    plt.plot(test_sameloc_list)
+    plt.plot(test_diffloc_list)
+    plt.show()
+    
+#    s, a, s_p = train[500]
+#    s = torch.from_numpy(s).float()
+#    s = s.unsqueeze(0)
+#    a = torch.from_numpy(a).float()
+#    a = a.unsqueeze(0)
+#    s_pred = model(s.cuda(), a.cuda())
+#    
+    
--- simple_rl/agents/func_approx/dsc/dynamics/rollouts.py
+++ simple_rl/agents/func_approx/dsc/dynamics/rollouts.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 
 from copy import deepcopy
@@ -16,26 +17,129 @@ class RolloutDataset(Dataset):
     
     def __getitem__(self, idx):
         return self.states[idx], self.actions[idx], self.states_p[idx]
+    
+class RandomRolloutCollector:
+    def __init__(self, epochs):
+         self.mdp = D4RLPointMazeMDP(difficulty="medium")
+         self._construct_train_set(epochs)
+    
+    def get_train_data(self):
+        return self.train
+    
+    def construct_data(self, epochs, random_position):
+        if random_position == True:
+            x_low, y_low = self.mdp.get_x_y_low_lims()
+            x_high, y_high = self.mdp.get_x_y_high_lims()
+            x_pos = random.uniform(x_low, x_high)
+            y_pos = random.uniform(y_low, y_high)
+            self.mdp.set_xy((x_pos, y_pos))
+        
+        buffer = []
+        for episode in range(epochs):
+          self.mdp.reset()
+          if random_position == True:
+              self.mdp.set_xy((x_pos, y_pos))
+    
+          for step in range(1000):
+            s = deepcopy(self.mdp.cur_state)
+            a = self.mdp.sample_random_action()
+            r, sp = self.mdp.execute_agent_action(a)
+            
+            x, y, theta, xdot, ydot, thetadot = s
+            x2, y2, theta2, xdot2, ydot2, thetadot2 = sp
+            
+            np_s = np.array([x, y, theta, xdot, ydot, thetadot])
+            np_sp = np.array([x2, y2, theta2, xdot2, ydot2, thetadot2])
+            
+            data = (np_s, a, np_sp - np_s)
+        
+            buffer.append(data)
+            if sp.is_terminal():
+                break
+        
+        # normalize data
+        states = []
+        actions = []
+        states_p = []
+        for state, action, state_p in buffer:
+            states.append(state)
+            actions.append(action)
+            states_p.append(state_p)
+        
+        dataset = RolloutDataset(states, actions, (states_p - self.mean_z) / self.std_z)
+        return dataset
+    
+    def get_standardization_vars(self):
+        return self.mean_x, self.mean_y, self.mean_z, self.std_x, self.std_y, self.std_z
+            
+    def _construct_train_set(self, epochs):
+        buffer = []
+        for episode in range(epochs):
+          self.mdp.reset()
+          for step in range(1000):
+            s = deepcopy(self.mdp.cur_state)
+            a = self.mdp.sample_random_action()
+            r, sp = self.mdp.execute_agent_action(a)
+            
+            x, y, theta, xdot, ydot, thetadot = s
+            x2, y2, theta2, xdot2, ydot2, thetadot2 = sp
+            
+            np_s = np.array([x, y, theta, xdot, ydot, thetadot])
+            np_sp = np.array([x2, y2, theta2, xdot2, ydot2, thetadot2])
+            
+            data = (np_s, a, np_sp - np_s)
+        
+            buffer.append(data)
+            if sp.is_terminal():
+                break
+        
+        # normalize data
+        states = []
+        actions = []
+        states_p = []
+        for state, action, state_p in buffer:
+            states.append(state)
+            actions.append(action)
+            states_p.append(state_p)
+            
+        self.mean_x = np.mean(states, axis=0)
+        self.mean_y = np.mean(actions, axis=0)
+        self.mean_z = np.mean(states_p, axis=0)
+        self.std_x = np.std(states - self.mean_x, axis=0)
+        self.std_y = np.std(actions - self.mean_y, axis=0)
+        self.std_z = np.std(states_p - self.mean_z, axis=0)
+        
+        dataset = RolloutDataset(states, actions, (states_p - self.mean_z) / self.std_z)
+        self.train = dataset
+        
 
-def collect_random_rollout(epochs=10):
+def collect_random_rollout(epochs=10, random_position=False):
     """
     TODO: add noise to inputs
     """
-    mdp = D4RLPointMazeMDP(seed=0, render=False)
-    # test set change start point
+    mdp = D4RLPointMazeMDP(seed=0, render=False, difficulty="medium")
+    if random_position == True:
+        x_low, y_low = mdp.get_x_y_low_lims()
+        x_high, y_high = mdp.get_x_y_high_lims()
+        x_pos = random.uniform(x_low, x_high)
+        y_pos = random.uniform(y_low, y_high)
+        mdp.set_xy((x_pos, y_pos))
     
     # collect data
     buffer = []
     
-    for episode in range(epochs):
+    for episode in range(10):
       mdp.reset()
+#      if random_position == True:
+#          mdp.set_xy((x_pos, y_pos))
+
       for step in range(1000):
         s = deepcopy(mdp.cur_state)
         a = mdp.sample_random_action()
         r, sp = mdp.execute_agent_action(a)
         
-        x, y, has_key, theta, xdot, ydot, thetadot = s
-        x2, y2, has_key2, theta2, xdot2, ydot2, thetadot2 = sp
+        x, y, theta, xdot, ydot, thetadot = s
+        x2, y2, theta2, xdot2, ydot2, thetadot2 = sp
         
         np_s = np.array([x, y, theta, xdot, ydot, thetadot])
         np_sp = np.array([x2, y2, theta2, xdot2, ydot2, thetadot2])
@@ -54,15 +158,35 @@ def collect_random_rollout(epochs=10):
         states.append(state)
         actions.append(action)
         states_p.append(state_p)
+        
+    states_mean = np.mean(states, axis=0)
+    states_std = np.std(states, axis=0)
+    states = states - states_mean
+    states = states / states_std
     
+    np.mean(states, axis=0)
+    np.std(states, axis=0)
+    
+    states_p_mean = np.mean(states_p, axis=0)
+    states_p = states_p - states_p_mean
+    states_p_std = np.std(states_p, axis=0)
+    states_p = states_p / states_p_std
+            
     state_scaler = preprocessing.StandardScaler().fit(states)
     action_scaler = preprocessing.StandardScaler().fit(actions)
     states_p_scaler = preprocessing.StandardScaler().fit(states_p)
     
+    states_p_scaler.mean_
+    states_p_scaler.var_
+    
     states = state_scaler.transform(states)
     actions = action_scaler.transform(actions)
-    states_p = states_p_scaler.transform(states_p)
-    
+    states_p = states_p_scaler.transform(states_p)    
     dataset = RolloutDataset(states, actions, states_p)
-    
+        
     return dataset
+
+if __name__== "__main__":
+    mdp = D4RLPointMazeMDP(seed=0, render=False, difficulty="medium")
+    mdp.reset()
+    type(mdp.cur_state.data)
