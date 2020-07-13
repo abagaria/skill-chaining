829963dc3a57695153f8b0cf55270742874216cf
kshitijsachan <kshitijsachan39@gmail.com>
2020-07-13 15:48:31 -0500

    now testing as well

--- simple_rl/agents/func_approx/dsc/DeepSkillGraphAgentClass.py
+++ simple_rl/agents/func_approx/dsc/DeepSkillGraphAgentClass.py
@@ -557,6 +557,6 @@ if __name__ == "__main__":
                                     plotter=mdp_plotter)
 
     num_successes = dsg_agent.dsg_run_loop(episodes=args.episodes, num_steps=args.steps)
-    # if args.generate_plots:
-    #     mdp_plotter.generate_final_experiment_plots(dsg_agent, args.episodes)
+    if args.generate_plots:
+        mdp_plotter.generate_final_experiment_plots(dsg_agent, args.episodes)
     ipdb.set_trace()
