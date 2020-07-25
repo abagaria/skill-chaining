9d64defc2e9499bf4ba2f119dd96d2e262b86f10
nktmerch <nicholas_merchant@brown.edu>
2020-07-24 20:22:06 -0400

    Debugging on policy

--- simple_rl/experiments/off_policy/on_policy_tester.py
+++ simple_rl/experiments/off_policy/on_policy_tester.py
@@ -67,7 +67,7 @@ def plot_value_function(directory: Path, solver: DDPGAgent) -> None:
 
     states_tensor = torch.from_numpy(states).float().to(solver.device)
     actions_tensor = torch.from_numpy(actions).float().to(solver.device)
-    qvalues = solver.get_qvalues(states_tensor, actions_tensor).cpu().numpy.squeeze(1)
+    qvalues = solver.get_qvalues(states_tensor, actions_tensor).cpu().numpy().squeeze(1)
     
     fig, ax = plt.subplots()
     scatter = ax.scatter(states[:, 0], states[:, 1], c = qvalues)
