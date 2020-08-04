8fc92c46a5a40f4fde3b2f56bb437837cedcdb23
kshitijsachan <kshitijsachan39@gmail.com>
2020-08-04 14:30:54 -0500

    value_function_colorbar

--- simple_rl/experiments/off_policy/off_policy_tester.py
+++ simple_rl/experiments/off_policy/off_policy_tester.py
@@ -212,7 +212,7 @@ class TrainOffPolicy:
         # plot scatter
         ax.scatter(x=positions[:, 0], y=positions[:, 1], color='b', alpha=0.16)
         self._plot_features(ax, goal_pos)
-        fig.suptitle(file_name)
+        fig.suptitle(replay_buffer.name)
 
         # save file
         plt.savefig(os.path.join(self.path, 'replay_buffers', file_name))
