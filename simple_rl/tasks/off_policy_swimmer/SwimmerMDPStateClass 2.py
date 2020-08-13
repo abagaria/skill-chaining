ff96d44874026762ab0093d0d85a454a85e233d0
kshitijsachan <kshitijsachan39@gmail.com>
2020-08-13 10:46:49 -0500

    reformatted whitespace

---
+++ simple_rl/tasks/off_policy_swimmer/SwimmerMDPStateClass 2.py
@@ -0,0 +1,26 @@
+# Python imports.
+import numpy as np
+
+# Other imports.
+from simple_rl.mdp.StateClass import State
+
+
+class SwimmerMDPState(State):
+    def __init__(self, position, others, is_terminal):
+        """
+        Args:
+            position (np.ndarray)
+            others (np.ndarray)
+            is_terminal (bool)
+        """
+        self.position = position
+        self.others = others
+        features = list(position) + list(others)
+
+        State.__init__(self, data=features, is_terminal=is_terminal)
+
+    def __str__(self):
+        return f"position: {self.position}, terminal: {self.is_terminal}"
+
+    def __repr__(self):
+        return str(self)
