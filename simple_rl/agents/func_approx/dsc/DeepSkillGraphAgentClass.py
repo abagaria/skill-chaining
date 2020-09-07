6c28a85a0c394a313d4e86f60b04e0d61c80017a
kshitijsachan <kshitijsachan39@gmail.com>
2020-09-07 13:22:49 -0500

    salient event factors bug

--- simple_rl/agents/func_approx/dsc/DeepSkillGraphAgentClass.py
+++ simple_rl/agents/func_approx/dsc/DeepSkillGraphAgentClass.py
@@ -138,7 +138,7 @@ class DeepSkillGraphAgent(object):
                     print(f"[DeepSkillGraphAgentClass] successfully reached MDP Goal State")
                     break
 
-            if self.plotter is not None and episode % 1000 == 0:
+            if self.plotter is not None and episode % 5 == 0:
                 self.plotter.generate_episode_plots(self.dsc_agent, episode)
 
         return successes
