de77997c964942e09030d1449000d8c63850806d
nktmerch <nicholas_merchant@brown.edu>
2020-09-25 13:35:32 -0400

    testing

--- simple_rl/agents/func_approx/ddpg/DDPGAgentClass.py
+++ simple_rl/agents/func_approx/ddpg/DDPGAgentClass.py
@@ -111,13 +111,18 @@ class DDPGAgent(Agent):
         return state_dictionary
 
     def __setstate__(self, state_dictionary):
-        self.name = state_dictionary["name"]
-        self.epsilon = state_dictionary["epsilon"]
-        self.state_size = state_dictionary["state_size"]
-        self.action_size = state_dictionary["action_size"]
-        self.device = state_dictionary["device"]
-        self.critic_learning_rate = state_dictionary["lr_critic"]
-        self.actor_learning_rate = state_dictionary["lr_actor"]
+        excluded_keys = [
+            "actor_state",
+            "actor_optimizer_state",
+            "critic_state",
+            "critic_optimizer_state",
+            "target_actor_state",
+            "target_critic_state"
+        ]
+
+        for x in state_dictionary.keys():
+            if x not in excluded_keys:
+                self.__dict__[x] = state_dictionary[x]
 
         self.actor = Actor(self.state_size, self.action_size, device=self.device)
         self.critic = Critic(self.state_size, self.action_size, device=self.device)
@@ -131,37 +136,8 @@ class DDPGAgent(Agent):
         self.target_actor.load_state_dict(state_dictionary["target_actor_state"])
         self.target_critic.load_state_dict(state_dictionary["target_critic_state"])
 
-        self.actor_optimizer.load_state_dict(state_dictionary["actor_optimizer"])
-        self.critic_optimizer.load_state_dict(state_dictionary["critic_optimizer"])
-
-        self.replay_buffer = state_dictionary["replay_buffer"]
-        # excluded_keys = [
-        #     "actor_state",
-        #     "actor_optimizer_state",
-        #     "critic_state",
-        #     "critic_optimizer_state",
-        #     "target_actor_state",
-        #     "target_critic_state"
-        # ]
-
-        # for x in state_dictionary.keys():
-        #     if x not in excluded_keys:
-        #         self.__dict__[x] = state_dictionary[x]
-
-        # self.actor = Actor(self.state_size, self.action_size, device=self.device)
-        # self.critic = Critic(self.state_size, self.action_size, device=self.device)
-        # self.target_actor = Actor(self.state_size, self.action_size, device=self.device)
-        # self.target_critic = Critic(self.state_size, self.action_size, device=self.device)
-        # self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=self.critic_learning_rate, weight_decay=1e-2)
-        # self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=self.actor_learning_rate)
-
-        # self.actor.load_state_dict(state_dictionary["actor_state"])
-        # self.critic.load_state_dict(state_dictionary["critic_state"])
-        # self.target_actor.load_state_dict(state_dictionary["target_actor_state"])
-        # self.target_critic.load_state_dict(state_dictionary["target_critic_state"])
-
-        # self.actor_optimizer.load_state_dict(state_dictionary["actor_optimizer_state"])
-        # self.critic_optimizer.load_state_dict(state_dictionary["critic_optimizer_state"])
+        self.actor_optimizer.load_state_dict(state_dictionary["actor_optimizer_state"])
+        self.critic_optimizer.load_state_dict(state_dictionary["critic_optimizer_state"])
         
 
     def add_noise_to_action(self, action):
