a79818eee2489b81ebb2d0f053f16691fefd1e8c
nktmerch <nicholas_merchant@brown.edu>
2020-12-11 15:38:53 -0700

    pairing down to single goal

--- simple_rl/experiments/off_policy/ddpg_with_her.py
+++ simple_rl/experiments/off_policy/ddpg_with_her.py
@@ -42,8 +42,8 @@ parser.add_argument('--goal_state', type=float, nargs='+')
 parser.add_argument('--dense_reward', action='store_true', default=False)
 parser.add_argument('--goal_reward', type=float, default=0.)
 parser.add_argument('--preload_solver_path', type=str, default=None)
-parser.add_argument('--test_time_start_states_pickle', type=str, default=None)
-parser.add_argument('--test_time_goal_states_pickle', type=str, default=None)
+#parser.add_argument('--test_time_start_states_pickle', type=str, default=None)
+#parser.add_argument('--test_time_goal_states_pickle', type=str, default=None)
 parser.add_argument('--intervention', type=str, default=None)
 
 args = parser.parse_args()
@@ -52,70 +52,14 @@ args = parser.parse_args()
 # PLOTTING AND PICKLING FUNCTIONS
 ################################################################################
 
-def visualize_ddpg_replay_buffer(solver, solver_number, directory):
-    print('*' * 80)
-    print("Plotting value function...")
-    print('*' * 80)
-    states = np.array([exp[0] for exp in solver.replay_buffer.memory])
-    actions = np.array([exp[1] for exp in solver.replay_buffer.memory])
-    states_tensor = torch.from_numpy(states).float().to(solver.device)
-    actions_tensor = torch.from_numpy(actions).float().to(solver.device)
-    qvalues = solver.get_qvalues(states_tensor, actions_tensor).cpu().numpy().squeeze(1)
-    plt.scatter(states[:, 0], states[:, 1], c=qvalues)
-
-    plt.xlim(-10, 10)
-    plt.ylim(-10, 10)
-
-    plt.xlabel("x position")
-    plt.ylabel("y position")
-
-    cbar = plt.colorbar()
-    cbar.set_label("predicted value")
-
-    plt.title(f"DDPG {solver_number}, unconditioned")
-
-    plt.savefig(directory / f'ddpg_value_function_{solver_number}.png')
-    plt.close()
-
-def visualize_gc_ddpg_replay_buffer(solver, goal_state, solver_number, directory):
-    print('*' * 80)
-    print("Plotting value function...")
-    print('*' * 80)
-    def condition_on_goal(state):
-        gc_state = state
-        gc_state[-2] = goal_state[0]
-        gc_state[-1] = goal_state[1]
-
-        return gc_state
-
-    states = np.array([condition_on_goal(exp[0]) for exp in solver.replay_buffer.memory])
-    actions = np.array([exp[1] for exp in solver.replay_buffer.memory])
-
-    states_tensor = torch.from_numpy(states).float().to(solver.device)
-    actions_tensor = torch.from_numpy(actions).float().to(solver.device)
-    qvalues = solver.get_qvalues(states_tensor, actions_tensor).cpu().numpy().squeeze(1)
-    plt.scatter(states[:, 0], states[:, 1], c=qvalues)
-
-    plt.xlim(-10, 10)
-    plt.ylim(-10, 10)
-
-    plt.xlabel("x position")
-    plt.ylabel("y position")
-
-    cbar = plt.colorbar()
-    cbar.set_label("predicted value")
-
-    plt.title(f"DDPG {solver_number}, conditioned on ({goal_state[0]},{goal_state[1]})")
-
-    plt.savefig(directory / f'ddpg_gc_value_function_{solver_number}.png')
-    plt.close()
-
 # def moving_average(a, n):
 #     ret = np.cumsum(a, dtype=float)
 #     ret[n:] = ret[n:] - ret[:-n]
 #     return ret[n-1:] / n
 
 if __name__ == '__main__':
+    ipdb.set_trace()
+    
     directory = Path.cwd() / f'{args.experiment_name}_{args.seed}'
     os.mkdir(directory)
 
@@ -157,7 +101,7 @@ if __name__ == '__main__':
                 mdp.state_space_size() + args.goal_dimension,
                 mdp.action_space_size(),
                 args.seed,
-                args.device,
+                torch.device(args.device),
                 name="pretrain"
                 )
         
@@ -181,128 +125,152 @@ if __name__ == '__main__':
     else:
         solver = pickle.load(open(args.preload_solver_path, 'rb'))
 
-    ipdb.set_trace()
+        new_device = torch.device(args.device)
+        solver.device = new_device
+        solver.actor.to(new_device)
+        solver.critic.to(new_device)
+        solver.target_actor.to(new_device)
+        solver.target_critic.to(new_device)
 
-    if args.test_time_start_states_pickle is not None:
-        test_time_start_states = pickle.load(open(args.test_time_start_states_pickle, 'rb'))
-    else:
-        test_time_start_states = [np.array([0,0])]
+        # # Initialize actor target network
+        # def copy_to_new_solver(new_params, old_params):
+        #     for new_param, param in zip(new_params, old_params):
+        #         target_param.data.copy_(param.data.to())
+
+        # # Initialize critic target network
+        # for target_param, param in zip(self.target_critic.parameters(), self.critic.parameters()):
+        #     target_param.data.copy_(param.data)
+        
+    # Got rid of this cos it's all good hai
+    # ipdb.set_trace()
+
+    # if args.test_time_start_states_pickle is not None:
+    #     test_time_start_states = pickle.load(open(args.test_time_start_states_pickle, 'rb'))
+    # else:
+    #     test_time_start_states = [np.array([0,0])]
     
-    if args.test_time_goal_states_pickle is not None:
-        test_time_goal_states = pickle.load(open(args.test_time_goal_states_pickle, 'rb'))
-    else:
-        test_time_goal_states = [np.array([3,3])]
+    # if args.test_time_goal_states_pickle is not None:
+    #     test_time_goal_states = pickle.load(open(args.test_time_goal_states_pickle, 'rb'))
+    # else:
+    #     test_time_goal_states = [np.array([2,2])]
 
-    for n, (start_state, goal_state) in enumerate(zip(test_time_start_states, test_time_goal_states)):    
-        # Each time we do a unique intervention, so have to copy
-        test_time_solver = deepcopy(solver)
-        test_time_solver.name = "test_time"
+    #for n, (start_state, goal_state) in enumerate(zip(test_time_start_states, test_time_goal_states)):    
 
-        # Just bad code right heeer
-        use_her = True
+    # Each time we do a unique intervention, so have to copy
+    #test_time_solver = deepcopy(solver)
+    #test_time_solver.name = "test_time"
 
-        # Run our intervention here
-        if args.intervention is not None:
-            if args.intervention == "empty_buffer":
-                test_time_solver.replay_buffer.clear()
+    start_state = np.array([0,0])
+    goal_state = np.array([2.5,2.5])
 
-                print("Fully emptied the replay buffer")
+    # Just bad code right heeer
+    use_her = True
 
-            elif args.intervention == "filter_buffer":
-                old_buffer = solver.replay_buffer
-                test_time_solver.replay_buffer.clear()
-                for state, action, reward, next_state, terminal in old_buffer:
-                    FILTER_THRESHOLD = 1
+    # Run our intervention here
+    if args.intervention is not None:
+        if args.intervention == "empty_buffer":
+            solver.replay_buffer.clear()
 
-                    # We are going to filter the replay buffer for states where
-                    # The conditioned goal state e.g. (state[-2],state[-1])
-                    # is within some threshold of our test-time goal state
-                    gc_state = state[-2:]
-                    if np.linalg.norm(gc_state - goal_state) <= FILTER_THRESHOLD:
-                        test_time_solver.replay_buffer.add(state, action, reward, next_state, terminal)
-                    
-                print(f"Filtered {test_time_solver.replay_buffer.num_exp} experiences out of {old_buffer.num_exp} with a threshold of {FILTER_THRESHOLD}")
+            print("Fully emptied the replay buffer")
 
-            elif args.intervention == "train_subnetwork":
-                # Only time we do this
-                use_her = False
+        elif args.intervention == "filter_buffer":
+            old_buffer = solver.replay_buffer
+            solver.replay_buffer.clear()
+            for state, action, reward, next_state, terminal in old_buffer:
+                FILTER_THRESHOLD = 1
 
-                # Here, we train a non-gc network on the fixed goal
-                test_time_solver = DDPGAgent(
-                    mdp.state_space_size(),
-                    mdp.action_space_size(),
-                    args.seed,
-                    args.device,
-                    name="test_time"
-                    )
+                # We are going to filter the replay buffer for states where
+                # The conditioned goal state e.g. (state[-2],state[-1])
+                # is within some threshold of our test-time goal state
+                gc_state = state[-2:]
+                if np.linalg.norm(gc_state - goal_state) <= FILTER_THRESHOLD:
+                    solver.replay_buffer.add(state, action, reward, next_state, terminal)
                 
-                def condition_on_goal(state):
-                    gc_state = state
-                    gc_state[-2:] = goal_state
+            print(f"Filtered {solver.replay_buffer.num_exp} experiences out of {old_buffer.num_exp} with a threshold of {FILTER_THRESHOLD}")
 
-                    return gc_state
+        elif args.intervention == "train_subnetwork":
+            # Only time we do this
+            use_her = False
 
-                old_buffer = solver.replay_buffer
-                # Even though we will strip the goal off this state,
-                # We need it to get the goal-conditioned predicted rewards
-                states = np.array([condition_on_goal(exp[0]) for exp in old_buffer.memory])
-                actions = np.array([exp[1] for exp in old_buffer.memory])
+            # Here, we train a non-gc network on the fixed goal
+            subnetwork = DDPGAgent(
+                mdp.state_space_size(),
+                mdp.action_space_size(),
+                args.seed,
+                torch.device(args.device),
+                name="test_time"
+                )
+            
+            def condition_on_goal(state):
+                gc_state = state
+                gc_state[-2:] = goal_state
 
-                # Here, we use the q value of the old solver as our ground truth
-                states_tensor = torch.from_numpy(states).float().to(args.device)
-                actions_tensor = torch.from_numpy(actions).float().to(args.device)
-                qvalues = solver.get_qvalues(states_tensor, actions_tensor).cpu().numpy().squeeze(1)
+                return gc_state
 
-                # Now, do the thing for next_state,next_action pairs
-                next_states = np.array([condition_on_goal(exp[3]) for exp in old_buffer.memory])
-                next_actions = np.array([solver.act(next_state) for next_state in next_states])
-                next_states_tensor = torch.from_numpy(next_states).float().to(args.device)
-                next_actions_tensor = torch.from_numpy(next_actions).float().to(args.device)
+            old_buffer = solver.replay_buffer
+            # Even though we will strip the goal off this state,
+            # We need it to get the goal-conditioned predicted rewards
+            states = np.array([condition_on_goal(exp[0]) for exp in old_buffer.memory])
+            actions = np.array([exp[1] for exp in old_buffer.memory])
 
-                next_qvalues = solver.get_qvalues(next_states_tensor, next_actions_tensor).cpu().numpy().squeeze(1)
-                discounted_next_qvalues = solver.gamma * next_qvalues
+            # Here, we use the q value of the old solver as our ground truth
+            states_tensor = torch.from_numpy(states).float().to(torch.device(args.device))
+            actions_tensor = torch.from_numpy(actions).float().to(torch.device(args.device))
+            qvalues = solver.get_qvalues(states_tensor, actions_tensor).cpu().numpy().squeeze(1)
 
-                rewards = qvalues - discounted_next_qvalues
+            # Now, do the thing for next_state,next_action pairs
+            next_states = np.array([condition_on_goal(exp[3]) for exp in old_buffer.memory])
+            next_actions = np.array([solver.act(next_state) for next_state in next_states])
+            next_states_tensor = torch.from_numpy(next_states).float().to(torch.device(args.device))
+            next_actions_tensor = torch.from_numpy(next_actions).float().to(torch.device(args.device))
 
-                def is_terminal(state):
-                    return np.linalg.norm(state[:2] - goal_state) <= args.goal_threshold
-                terminals = [is_terminal(next_state) for next_state in next_states]
+            next_qvalues = solver.get_qvalues(next_states_tensor, next_actions_tensor).cpu().numpy().squeeze(1)
+            discounted_next_qvalues = solver.gamma * next_qvalues
 
-                # We can then train on these q values
-                for state, action, reward, next_state, terminal in zip(states, actions, rewards, next_states, terminals):
-                    # !!! important, our new solver is NOT goal conditioned
-                    state = state[:-2]
-                    next_state = next_state[:-2]
+            rewards = qvalues - discounted_next_qvalues
 
-                    reward = float(reward)
-                    terminal = bool(terminal)
+            def is_terminal(state):
+                return np.linalg.norm(state[:2] - goal_state) <= args.goal_threshold
 
-                    test_time_solver.step(state, action, reward, next_state, terminal)
+            terminals = [is_terminal(next_state) for next_state in next_states]
 
+            # We can then train on these q values
+            for state, action, reward, next_state, terminal in zip(states, actions, rewards, next_states, terminals):
+                # !!! important, our new solver is NOT goal conditioned
+                state = state[:-2]
+                next_state = next_state[:-2]
 
-            else:
-                raise NotImplementedError(f"{args.intervention} is not a valid intervention")
-        # TODO: Change this to not use HER if our intervention
-        # was train subnetwork -- in that case, we now have a 
-        # fixed-goal policy
+                reward = float(reward)
+                terminal = bool(terminal)
 
-        # (ii) Test on a fixed-goal domain, maybe pretrained
-        pes_ii, ped_ii, pesuccesses_ii, _ = her_train(
-            test_time_solver,
-            mdp,
-            args.num_episodes,
-            args.num_steps,
-            start_state = start_state,
-            goal_state = goal_state,
-            sampling_strategy="test",
-            dense_reward=args.dense_reward,
-            use_her=use_her
-            )
+                subnetwork.step(state, action, reward, next_state, terminal)
 
-        pickle.dump(pes_ii, open(directory / f'test_time_scores_{n}.pkl', 'wb'))
-        pickle.dump(ped_ii, open(directory / f'test_time_durations_{n}.pkl', 'wb'))
-        pickle.dump(pesuccesses_ii, open(directory / f'test_time_successes_{n}.pkl', 'wb'))
-        pickle.dump(test_time_solver, open(directory / f'ddpg_{test_time_solver.name}_{n}.pkl', 'wb'))
+            # Here we drop the teacher
+            solver = subnetwork
+
+        else:
+            raise NotImplementedError(f"{args.intervention} is not a valid intervention")
+    # TODO: Change this to not use HER if our intervention
+    # was train subnetwork -- in that case, we now have a 
+    # fixed-goal policy
+
+    # (ii) Test on a fixed-goal domain, maybe pretrained
+    pes_ii, ped_ii, pesuccesses_ii, _ = her_train(
+        solver,
+        mdp,
+        args.num_episodes,
+        args.num_steps,
+        start_state = start_state,
+        goal_state = goal_state,
+        sampling_strategy="test",
+        dense_reward=args.dense_reward,
+        use_her=use_her
+        )
+
+    pickle.dump(pes_ii, open(directory / f'test_time_scores_{n}.pkl', 'wb'))
+    pickle.dump(ped_ii, open(directory / f'test_time_durations_{n}.pkl', 'wb'))
+    pickle.dump(pesuccesses_ii, open(directory / f'test_time_successes_{n}.pkl', 'wb'))
+    pickle.dump(solver, open(directory / f'ddpg_{solver.name}_{n}.pkl', 'wb'))
 
     ipdb.set_trace()
 
