a00b39d5f384419a79473382a21f0bc1fbfb79ec
Akhil Bagaria <abagaria@hmc.edu>
2021-04-13 16:33:38 -0400

    [SalientEventClass] - Add support for SalientEvents that dont target a state, but just a predicate (eg key) - Add support for SalientEvents that are image based - Use EMD as a metric between images - Removed the unused SalientEvent classes

--- simple_rl/agents/func_approx/dsc/SalientEventClass.py
+++ simple_rl/agents/func_approx/dsc/SalientEventClass.py
@@ -1,3 +1,5 @@
+import cv2
+import time
 import numpy as np
 from simple_rl.mdp.StateClass import State
 from scipy.spatial import distance
@@ -8,24 +10,25 @@ from collections import deque
 
 
 class SalientEvent(object):
-    def __init__(self, target_state, event_idx, tolerance=0.6, intersection_event=False, is_init_event=False):
+    def __init__(self, target_state, event_idx, predicate=None, tolerance=2.,
+                 use_position=False, is_init_event=False):
+        self.predicate = predicate
         self.target_state = target_state
         self.event_idx = event_idx
         self.tolerance = tolerance
-        self.intersection_event = intersection_event
+        self.use_position = use_position
         self.is_init_event = is_init_event
 
         # This is the union of the effect set of all the options targeting this salient event
-        self.trigger_points = []
-        self._initialize_trigger_points()
-
+        self.trigger_points = deque(maxlen=50)
         self.revised_by_mpc = False
 
         assert isinstance(event_idx, int)
         assert isinstance(tolerance, float)
 
-    def _initialize_trigger_points(self):
-        self.trigger_points = [self.target_state]
+    def add_trigger_point(self, trigger_point):
+        assert isinstance(trigger_point, State)
+        self.trigger_points.append(trigger_point)
 
     def __call__(self, states):
         """
@@ -52,9 +55,11 @@ class SalientEvent(object):
         if not isinstance(other, SalientEvent):
             return False
 
-        return _state_eq(self.target_state, other.target_state) and \
-               self.tolerance == other.tolerance # and \
-               # self.event_idx == other.event_idx
+        if self.use_position:
+            return _state_eq(self.target_state, other.target_state) \
+                    and self.tolerance == other.tolerance
+
+        return self.event_idx == other.event_idx
 
     def __hash__(self):
         return hash(self.event_idx)
@@ -76,6 +81,11 @@ class SalientEvent(object):
         Returns:
             distance (float)
         """
+        if self.use_position:
+            return self._position_based_distance(other)
+        return self._emd_based_distance(other)
+
+    def _position_based_distance(self, other):
         point1 = self.get_target_position()
 
         if other.get_target_position() is not None:
@@ -83,6 +93,12 @@ class SalientEvent(object):
             return self.point_to_point_distance(point1, point2)
 
         return self.point_to_set_distance(point1, other.trigger_points)
+
+    def _emd_based_distance(self, e2):
+        g = e2.trigger_points[0].features()
+        s = random.choice(self.trigger_points).features()
+        d = self.emd_between_images(self.extract(s), self.extract(g))
+        return d
 
     def distance_to_state(self, state):
         return self.distance_to_effect_set([state])
@@ -103,8 +119,42 @@ class SalientEvent(object):
         Returns:
             distance (float)
         """
-        point = self._get_position(self.target_state)
-        return self.point_to_set_distance(point, effect_set)
+        if self.use_position or len(self.trigger_points) == 0:
+            point = self._get_position(self.target_state)
+            return self.point_to_set_distance(point, effect_set)
+
+        effect_point = random.choice(effect_set)
+        trigger_point = random.choice(self.trigger_points)
+
+        effect_image = self.extract(effect_point.features())
+        trigger_image = self.extract(trigger_point.features())
+
+        emd = self.emd_between_images(effect_image, trigger_image)
+        return emd
+
+    @staticmethod
+    def emd_between_images(image1, image2):
+        image1 = cv2.resize(image1, (30, 30), interpolation=cv2.INTER_AREA)
+        image2 = cv2.resize(image2, (30, 30), interpolation=cv2.INTER_AREA)
+        sig1 = SalientEvent.img_to_sig(image1)
+        sig2 = SalientEvent.img_to_sig(image2)
+        dist, _, _ = cv2.EMD(sig1, sig2, cv2.DIST_L2)
+        return dist
+
+    @staticmethod
+    def img_to_sig(arr):
+        """ Create a signature of the input image for EMD calculation. """
+        sig = np.empty((arr.size, 3), dtype=np.float32)
+        count = 0
+        for i in range(arr.shape[0]):
+            for j in range(arr.shape[1]):
+                sig[count] = np.array([arr[i,j], i, j])
+                count += 1
+        return sig
+
+    @staticmethod
+    def extract(i):
+        return np.array(i)[-1, ...]
 
     @staticmethod
     def point_to_point_distance(point1, point2):
@@ -134,15 +184,25 @@ class SalientEvent(object):
         return distances.max()
 
     def is_init_true(self, state):
+        if self.predicate is not None:
+            return self.predicate(state)
+
         position = self._get_position(state)
         target_position = self._get_position(self.target_state)
         dist = np.linalg.norm(position - target_position)
         return np.round(dist, 8) <= self.tolerance
 
     def pessimistic_is_init_true(self, state):
+        if self.predicate is not None:
+            return self.predicate(state)
         return self.is_init_true(state)
 
-    def batched_is_init_true(self, position_matrix):
+    def batched_is_init_true(self, state_set):
+
+        if self.predicate is not None:
+            return np.array([self.is_init_true(state) for state in state_set])
+        
+        position_matrix = self._batched_get_position(state_set)
         assert isinstance(position_matrix, np.ndarray), type(position_matrix)
         goal_position = self._get_position(self.target_state)
         distances = distance.cdist(position_matrix, goal_position[None, :])
@@ -154,8 +214,9 @@ class SalientEvent(object):
 
     @staticmethod
     def _get_position(state):
-        position = state.position if isinstance(state, State) else state[:2]
-        assert isinstance(position, np.ndarray), type(position)
+        position = state.get_position() if isinstance(state, State) else state[:2]  # TODO: state[:2] for ant --- automate!
+        if not isinstance(position, np.ndarray):
+            position = np.array(position)
         return position
 
     @staticmethod
@@ -167,130 +228,3 @@ class SalientEvent(object):
 
     def __repr__(self):
         return f"SalientEvent targeting {self.target_state}"
-
-
-class LearnedSalientEvent(SalientEvent):
-    def __init__(self, state_set, event_idx, tolerance=0.6, intersection_event=False):
-        self.state_set = state_set
-        self.classifier = self._classifier_on_state_set()
-
-        SalientEvent.__init__(self, target_state=None, event_idx=event_idx,
-                              tolerance=tolerance, intersection_event=intersection_event)
-
-    def __repr__(self):
-        return f"LearnedSalientEvent {self.event_idx}"
-
-    def __hash__(self):
-        return hash(self.event_idx)
-
-    def get_target_position(self):
-        return None
-
-    def _initialize_trigger_points(self):
-        self.trigger_points = [state.position for state in self.state_set]
-
-    def is_init_true(self, state):
-        position = self._get_position(state)
-        return self.classifier.predict(position.reshape(1, -1))
-
-    def batched_is_init_true(self, position_matrix):
-        assert isinstance(position_matrix, np.ndarray), type(position_matrix)
-        return self.classifier.predict(position_matrix)
-
-    def __eq__(self, other):
-        if not isinstance(other, SalientEvent):
-            return False
-        return self.event_idx == other.event_idx and self.tolerance == other.tolerance
-
-    def _classifier_on_state_set(self):
-        positions = np.array([state.position for state in self.state_set])
-        classifier = OneClassSVM(nu=0.01, gamma="scale")
-        classifier.fit(positions)
-        return classifier
-
-    def distance_to_effect_set(self, effect_set):
-        """ Compute the max distance from `state_set` to `effect_set`. """
-        return self.set_to_set_distance(self.state_set, effect_set)
-
-    def distance_to_other_event(self, other):
-        if other.get_target_position() is None:
-            return self.set_to_set_distance(self.state_set, other.trigger_points)
-        return self.point_to_set_distance(other.get_target_position(), self.state_set)
-
-
-class DCOSalientEvent(SalientEvent):
-    def __init__(self, covering_option, event_idx, is_low, tolerance=0.6, intersection_event=False):
-        self.covering_option = covering_option
-        self.is_low = is_low
-
-        target_state = self.covering_option.min_f_value_state if is_low else self.covering_option.max_f_value_state
-
-        SalientEvent.__init__(self, target_state=target_state, event_idx=event_idx,
-                              tolerance=tolerance, intersection_event=intersection_event)
-
-    def __eq__(self, other):
-        if not isinstance(other, SalientEvent):
-            return False
-        return self is other
-
-    def __repr__(self):
-        return f"DCOSalientEvent {self.event_idx} targeting {self.target_state}"
-
-    def __hash__(self):
-        return self.event_idx
-
-    def distance_to_other_event(self, other):
-        if other.get_target_position() is not None:
-            return super(DCOSalientEvent, self).distance_to_other_event(other)
-        return self.point_to_set_distance(self.get_target_position(), other.trigger_points)
-
-
-class DSCOptionSalientEvent(SalientEvent):
-    def __init__(self, option, event_idx, tolerance=0.6):
-        """
-
-        Args:
-            option (Option)
-        """
-        self.option = option
-        self._initialize_trigger_points()
-
-        SalientEvent.__init__(self,
-                              target_state=None,
-                              event_idx=event_idx,
-                              tolerance=tolerance,
-                              intersection_event=False)
-
-    def _initialize_trigger_points(self):
-        self.trigger_points = self.option.effect_set
-
-    def is_init_true(self, state):
-        return self.option.is_init_true(state)
-
-    def batched_is_init_true(self, position_matrix):
-        self.option.batched_is_init_true(position_matrix)
-
-    def __eq__(self, other):
-        return self is other
-
-    def __hash__(self):
-        return self.event_idx
-
-    def __repr__(self):
-        return f"SalientEvent corresponding to {self.option}"
-
-    def get_target_position(self):
-        return None
-
-    @staticmethod
-    def _get_position(state):
-        return None
-
-    def distance_to_effect_set(self, effect_set):
-        return self.set_to_set_distance(self.trigger_points, effect_set)
-
-    def distance_to_other_event(self, other):
-        if other.get_target_position() is not None:
-            return self.point_to_set_distance(other.get_target_position(), self.trigger_points)
-        return self.set_to_set_distance(self.trigger_points, other.trigger_points)
-
